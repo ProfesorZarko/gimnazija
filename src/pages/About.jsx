55bb6d7132773887356ac6589d221a9fcfed5288
ProfesorZarko <profesorfz2@gmail.com>
2025-05-20 08:47:14 +0200

    inicijala za gimnaziju 01

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>About
+        <h1>About Us</h1>
+        <p>This is the about page of our application.</p>
+        <p>We are dedicated to providing the best service possible.</p>
+        <p>Thank you for visiting!</p>
+    </div>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>Home
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page of our application.</p>
+        <p>Feel free to explore!</p>
+    </div>
+  )
+}
